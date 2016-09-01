219a224084cbee3185eefa9b12ca7f4b58bc6749
emma <emmaruthoneill@gmail.com>
2016-09-01 15:52:41 -0700

    making the map mobile responsive

--- src/js/treasureisland.js
+++ src/js/treasureisland.js
@@ -11,7 +11,6 @@ var bodyvar = document.querySelector('body');
 
 if (screen.width > 320) {
   document.querySelector('#expandable-photo').addEventListener('click', function(){
-    console.log("clicked on expandable");
     bigphoto.classList.add("expanded");
     bodyvar.classList.add("expanded");
   });
@@ -30,34 +29,12 @@ if (screen.width > 320) {
   results = [];
 });
 
-if (screen.width > 480) {
-  var mapImage = document.getElementById("ti-image")
-  var mapHeight = 1535;//mapImage.getAttribute("height")+40;
-  console.log(mapHeight);
-//   // var mapHeight = 1200 + 40; //size of map + 40 pixels of padding
-  // var textHeight = 0;
-  var inc = mapHeight/4;
-  console.log(inc); //how often we should see new map element
-  // [0,1,2,3].forEach(function(d) {
-  //   var loc_str = "#development"+d;
-  //   textHeight = textHeight + document.getElementById(loc_str).getAttribute("height")+40; // each block has 40px padding
-  //   console.log(textHeight);
-  // });
-  // textHeight = textHeight-20; // every block has 40px of padding except the bottom
+var mapHeight = $("#ti-image").height();
+$(".flex-map").css("height",mapHeight+20+"px")
 
-  // var top_padding = Math.floor((mapHeight-textHeight)/7);
-// //  console.log(top_padding);
-//   if (top_padding < 0) {
-// //    console.log("ERROR ERROR ERROR");
-//     top_padding = 0;
-//   }
-//   embarcaderoData.forEach(function(pier,index) {
-//     if (index > 0) {
-//       var pier_str = ".pier"+index;
-//       $(pier_str).css('padding-top',top_padding);
-//     }
-//   });
-//
+if (screen.width > 480) {
+  var mapHeight = $("#ti-image").height();
+  var inc = mapHeight/4;
   $(window).scroll(function(){
       var pos = $(this).scrollTop();
       var shoreline_pos = $('#sticky-development-map-top').offset().top-200;
@@ -76,18 +53,11 @@ if (screen.width > 480) {
         if (idx < 4 && idx >= 0) {
           var mapID = document.getElementById("development"+idx);
           mapID.classList.add("active");
-          var inc_new = Math.round(inc_list[idx]);
+          var inc_new = Math.round(inc_list[idx]*mapHeight/1535);
           // var top_padding = (idx*inc+10)+"px";
           var top_padding = inc_new+"px";
           $("#development"+idx).css('padding-top',top_padding);
         }
-        // $(".pier-info").css('color','#B2B2B2');
-        // $('.st').css('fill','#F2F2F2');
-        // var idx = Math.round((pos-embarcadero_pos)/inc);
-        // var pier_active = ".pier"+idx;
-        // var pier_path_active = ".st"+idx;
-        // $(pier_active).css('color','black');
-        // $(pier_path_active).css('fill', 'red');
       }
   });
 }
