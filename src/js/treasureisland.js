c054ee588034d1b2ffc5a7c3945253cf7bfc2959
emma <emmaruthoneill@gmail.com>
2016-09-01 18:04:47 -0700

    map is almost done

--- src/js/treasureisland.js
+++ src/js/treasureisland.js
@@ -24,17 +24,27 @@ if (screen.width > 320) {
 [0,1,2,3].forEach(function(d,idx){
   var mapID = document.getElementById("development"+d);
   var results = mapData[d];
-  var html = "<div class='map-title'>"+results.title+"</div><div class='map-hed'>What's there now: </div><div class='map-entry'>"+results.therenow+"</div><div class='map-hed'>What's planned: </div><div class='map-entry'>"+results.planned+"</div><div class='map-hed'>Protective measures: </div><div class='map-entry'>"+results.protective+"</div><div class='map-hed'>Timetable: </div><div class='map-entry'>"+results.timetable+"</div>";
+  var html = "<div class='map-image'><img class='img' src='"+results.img+"'></img></div><div class='map-title'>"+results.title+"</div><div class='map-hed'>What's there now: </div><div class='map-entry'>"+results.therenow+"</div><div class='map-hed'>What's planned: </div><div class='map-entry'>"+results.planned+"</div><div class='map-hed'>Protective measures: </div><div class='map-entry'>"+results.protective+"</div><div class='map-hed'>Timetable: </div><div class='map-entry'>"+results.timetable+"</div>";
   mapID.innerHTML = html;
   results = [];
 });
 
 var mapHeight = $("#ti-image").height();
-$(".flex-map").css("height",mapHeight+20+"px")
+$(".flex-map").css("height",mapHeight+40+"px")
+if (screen.width < 768){
+  var inc_list = [0,200,400,500];
+} else {
+  var inc_list = [0,250,450,650];
+}
+
+function fill_mobile_template(results,mobileID){
+  var html = "<div class='map-image'><img class='img' src='"+results.img+"'></img></div><div class='map-title'>"+results.title+"</div><div class='map-hed'>What's there now: </div><div class='map-entry'>"+results.therenow+"</div><div class='map-hed'>What's planned: </div><div class='map-entry'>"+results.planned+"</div><div class='map-hed'>Protective measures: </div><div class='map-entry'>"+results.protective+"</div><div class='map-hed'>Timetable: </div><div class='map-entry'>"+results.timetable+"</div>";
+  mobileID.innerHTML = html;
+}
 
 if (screen.width > 480) {
   var mapHeight = $("#ti-image").height();
-  var inc = mapHeight/4;
+  var inc = mapHeight/5;
   $(window).scroll(function(){
       var pos = $(this).scrollTop();
       var shoreline_pos = $('#sticky-development-map-top').offset().top-200;
@@ -42,22 +52,62 @@ if (screen.width > 480) {
           document.getElementById('development0').classList.add("active");
       }
       if(pos > shoreline_pos) {
-        var inc_list = [0,500,800,1000];
         [0,1,2,3].forEach(function(d,idx){
           var mapID = document.getElementById("development"+d);
           mapID.classList.remove("active");
         });
         var idx = Math.round((pos-shoreline_pos)/inc);
-        console.log(idx);
 
         if (idx < 4 && idx >= 0) {
           var mapID = document.getElementById("development"+idx);
           mapID.classList.add("active");
           var inc_new = Math.round(inc_list[idx]*mapHeight/1535);
-          // var top_padding = (idx*inc+10)+"px";
           var top_padding = inc_new+"px";
           $("#development"+idx).css('padding-top',top_padding);
+        } else {
+          var inc_new = Math.round(inc_list[3]*mapHeight/1535);
+          var top_padding = inc_new+"px";
+          $("#development3").css('padding-top',top_padding);
+          document.getElementById('development3').classList.add("active");
         }
       }
+  });
+} else {
+  // initialize map interactive on mobile
+  var index = 0;
+  var mobileID = document.getElementById("mobile-development");
+  var results = mapData[index];
+  fill_mobile_template(results,mobileID);
+  // set up clicking to update map interactive on mobile
+  document.getElementById("scroll-right-dev").addEventListener("click", function() {
+    index = index+1;
+    var results = mapData[index];
+    fill_mobile_template(results,mobileID)
+    if (index == 3) {
+      $("#scroll-right-dev").addClass("last");
+    } else {
+      $("#scroll-right-dev").removeClass("last");
+    };
+    if (index == 0) {
+      $("#scroll-left-dev").addClass("first");
+    } else {
+      $("#scroll-left-dev").removeClass("first");
+    };
+  });
+
+  document.getElementById("scroll-left-dev").addEventListener("click", function() {
+    index = index-1;
+    var results = mapData[index];
+    fill_mobile_template(results,mobileID)
+    if (index == 0) {
+      $("#scroll-left-dev").addClass("first");
+    } else {
+      $("#scroll-left-dev").removeClass("first");
+    };
+    if (index == 3) {
+      $("#scroll-right-dev").addClass("last");
+    } else {
+      $("#scroll-right-dev").removeClass("last");
+    };
   });
 }
